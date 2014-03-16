eb1f89886bb0aad5c85526765bdadd600c87578b
Tim Simmons <tim.r.simmons@gmail.com>
2014-03-16 17:58:21 -0500

    Add a Basic Framwork for Serving HTML Pages with Django Templates

---
+++ idb/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+import datetime
+from django.http import HttpResponse
+from data import *
+
+def home(request):
+    return render(request, "index.html")
+
+def player(request, player_id):
+    if player_id == "01":
+        return render(request, "player.html", {"player": bryce} )
+    return render(request, "player.html")
+
+def team(request, team_id):
+    if team_id == "20":
+        return render(request, "team.html", {"team": angels} )
+    return render(request, "team.html")
+
+def year(request, year_id):
+    if year_id == "2013":
+        return render(request, "year.html", {"year": year_2013} )
+    return render(request, "year.html")
+
