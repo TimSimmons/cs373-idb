87f3a2f0648980dffd05492f427642df106fbb87
Kendall Ahrendsen <kendall@cs.utexas.edu>
2014-03-18 17:46:35 -0500

    added placeholder twitter feed, issue with social attribute of players causes data-screen-name to misbehave

--- idb/views.py
+++ idb/views.py
@@ -8,16 +8,17 @@ def home(request):
 
 def player(request, player_id):
     if player_id == "01":
-        return render(request, "player.html", {"player": bryce} )
+        player.social=player.social[1:]
+        return render(request, "player.html", bryce)
     return render(request, "player.html")
 
 def team(request, team_id):
     if team_id == "20":
-        return render(request, "team.html", {"team": angels} )
+        return render(request, "team.html", angels)
     return render(request, "team.html")
 
 def year(request, year_id):
     if year_id == "2013":
-        return render(request, "year.html", {"year": year_2013} )
+        return render(request, "year.html", year_2013 )
     return render(request, "year.html")
 
