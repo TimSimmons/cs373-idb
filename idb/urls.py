eec6e24a82e6d52db2c158a11863238eef1575cc
Tim Simmons <tim.r.simmons@gmail.com>
2014-03-30 14:42:59 -0500

    Add preliminary player page to be served by model
    Serve player model from the database. Keeping it in a sepearate template/view/url for now.

--- idb/urls.py
+++ idb/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from idb.views import home, player, team, year
+from idb.views import home, player, team, year, player_model
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,6 +10,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', home),
     url(r'^players/(\d{2})/$', player),
+    url(r'^players_model/(\d{1})/$', player_model),
     url(r'^teams/([A-Za-z]{3})/$', team),
     url(r'^years/(\d{4})/$', year),
     url(r'^admin/', include(admin.site.urls)),
