676ec61442c08e76cc047eaa2105e23630f2699e
Jordan Graves <jgraves8205@aol.com>
2014-04-02 17:16:48 -0500

    Populate the database

--- idb/data/players.py
+++ idb/data/players.py
@@ -1,21 +1,21 @@
 #Players
-bryce_year1 = {"year": "2012", "team": "Washington Nationals", "type": "hitter", "games": "139", "pa": "597", "avg": ".270", "obp": ".340", "slg": ".477", "hr": "22", "rbi": "59", "player_id": "3" }
-bryce_year2 = {"year": "2013", "team": "Washington Nationals", "type": "hitter", "games": "118", "pa": "497", "avg": ".274", "obp": ".368", "slg": ".486", "hr": "20", "rbi": "58", "player_id": "3"}
-bryce_player = { "name": "Bryce Harper", "number": "34", "position": "OF", "number": "34",  "social": "@Bharper3407" ,"bats": "R", "throws": "L", "height": "74", "weight": "230", "school": "College of Southern Nevada"}
+bryce_year1 = {"year": "2012", "team": "Washington Nationals", "kind": "hitter", "games": 139, "pa": 597, "avg": .270, "obp": .340, "slg": .477, "hr": 22, "rbi": 59 }
+bryce_year2 = {"year": "2013", "team": "Washington Nationals", "kind": "hitter", "games": 118, "pa": 497, "avg": .274, "obp": .368, "slg": .486, "hr": 20, "rbi": 58 }
+bryce_player = { "name": "Bryce Harper", "number": 34, "position": "OF",  "social": "@Bharper3407" ,"bats": "R", "throws": "L", "height": 74, "weight": 230, "school": "College of Southern Nevada"}
 bryce_image = "http://mlb.com/images/players/525x330/547180.jpg"
 bryce_years = [bryce_year1, bryce_year2]
 
-yu_year1 = {"year": "2012", "team": "TEX", "type": "pitcher", "games": "29", "w": "16", "l": "9", "era": "3.90", "gs": "29", "s": "0", "whip": "1.280", "ip": "191.1"}
-yu_year2 = { "year": "2013", "team": "TEX", "type": "pitcher", "games": "32", "w": "13", "l": "9", "era": "2.83", "gs": "32", "s": "0", "whip": "1.073", "ip": "209.2"}
+yu_year1 = {"year": "2012", "team": "Texas Rangers", "kind": "pitcher", "games": 29, "w": 16, "l": 9, "era": 3.90, "gs": 29, "s": 0, "whip": 1.280, "ip": 191.1}
+yu_year2 = { "year": "2013", "team": "Texas Rangers", "kind": "pitcher", "games": 32, "w": 13, "l": 9, "era": 2.83, "gs": 32, "s": 0, "whip": 1.073, "ip": 209.2}
 yu_image = "http://mlb.com/images/players/525x330/506433.jpg"
-yu_player = { "name": "Yu Darvish", "number": "11", "position": "P", "number": "11", "social": "@faridyu" ,"bats": "R", "throws": "R", "height": "6'5", "weight": "225", "school": "Sendai, Japan", "years": [yu_year1, yu_year2] }
+yu_player = { "name": "Yu Darvish", "number": 11, "position": "P", "social": "@faridyu" ,"bats": "R", "throws": "R", "height": 77, "weight": 225, "school": "Sendai, Japan" }
 yu_years = [yu_year1, yu_year2]
 
-mike_year1 = {"year": "2011", "team": "Los Angeles Angels of Anaheim", "type": "hitter", "games": "40", "pa": "135", "avg": ".220", "obp": ".281", "slg": ".390", "hr": "5", "rbi": "16", "player_id": "10"}
-mike_year2 = {"year": "2012", "team": "Los Angeles Angels of Anaheim", "type": "hitter", "games": "139", "pa": "639", "avg": ".326", "obp": ".399", "slg": ".564", "hr": "30", "rbi": "83", "player_id": "10"}
-mike_year3 = {"year": "2013", "team": "Los Angeles Angels of Anaheim", "type": "hitter", "games": "157", "pa": "716", "avg": ".323", "obp": ".432", "slg": ".557", "hr": "27", "rbi": "97", "player_id": "10"}
+mike_year1 = {"year": "2011", "team": "Los Angeles Angels of Anaheim", "kind": "hitter", "games": 40, "pa": 135, "avg": .220, "obp": .281, "slg": .390, "hr": 5, "rbi": 16 }
+mike_year2 = {"year": "2012", "team": "Los Angeles Angels of Anaheim", "kind": "hitter", "games": 139, "pa": 639, "avg": .326, "obp": .399, "slg": .564, "hr": 30, "rbi": 83 }
+mike_year3 = {"year": "2013", "team": "Los Angeles Angels of Anaheim", "kind": "hitter", "games": 157, "pa": 716, "avg": .323, "obp": .432, "slg": .557, "hr": 27, "rbi": 97 }
 mike_image = "http://mlb.com/images/players/525x330/545361.jpg"
-mike_player = { "name": "Mike Trout", "number": "27", "position": "OF", "number": "27", "social": "@Trouty20" ,"bats": "R", "throws": "R", "height": "6'2", "weight": "230", "school": "Millville Senior HS, Millville, NJ" }
+mike_player = { "name": "Mike Trout", "number": 27, "position": "OF", "social": "@Trouty20" ,"bats": "R", "throws": "R", "height": 74, "weight": 230, "school": "Millville Senior HS, Millville, NJ" }
 mike_years = [mike_year1, mike_year2, mike_year3]
 
 bryce = dict(player=bryce_player, image=bryce_image, years=bryce_years)
@@ -23,4 +23,4 @@ mike = dict(player=mike_player, image=mike_image, years=mike_years)
 yu = dict(player=yu_player, image=yu_image, years=yu_years)
 
 # Leave out yu because we don't have a Texas team in the DB
-players = [bryce, mike]
+players = [bryce, mike, yu]
--- idb/data/teams.py
+++ idb/data/teams.py
@@ -1,5 +1,5 @@
 #Teams
-angels_team = { "id": "1", "name": "Los Angeles Angels of Anaheim", 
+angels_team = { "abbr": "LAA", "name": "Los Angeles Angels of Anaheim", 
 	   "social": "@Angels", "city": "Los Angeles", "state": "CA", 
 	   "park": "Angel Stadium of Anaheim", 
 	   "div": "AL West", "mgr": "Mike Scioscia"
@@ -16,7 +16,7 @@ angels_years = [{ "year": "2013", "wins": 78, "losses": 84, "standing": 3, "play
 		{ "year": "2005", "wins": 95, "losses": 67, "standing": 1, "playoffs": "Lost ALCS (4-1)", "attend": 3404686, "payroll": 94867822},
 		{ "year": "2004", "wins": 92, "losses": 70, "standing": 1, "playoffs": "Lost LDS (3-0)", "attend": 3375677, "payroll": 100534667} ]
 	     
-cardinals_team = { "id": "2", "name": "St. Louis Cardinals", 
+cardinals_team = { "abbr": "STL", "name": "St. Louis Cardinals", 
 	      "social": "@Cardinals", "city": "St. Louis", "state": "MO", 
 	      "park": "Busch Stadium III", 
 	      "div": "NL Central", "mgr": "Mike Matheny"} 
@@ -32,7 +32,7 @@ cardinals_years = [{ "year": "2013", "wins": 97, "losses": 65, "standing": 1, "p
 		   { "year": "2005", "wins": 100, "losses": 62, "standing": 1, "playoffs": "Lost NLCS (4-2)", "attend": 3538988, "payroll": 92106833 },
 		   { "year": "2004", "wins": 105, "losses": 57, "standing": 1, "playoffs": "Lost WS (4-0)", "attend": 3048427, "payroll": 84340333}]
 		   
-dodgers_team = { "id": "3", "name": "Los Angeles Dodgers", 
+dodgers_team = { "abbr": "LAD", "name": "Los Angeles Dodgers", 
 	    "social": "@Dodgers", "city": "Los Angeles", "state": "CA", 
 	    "park": "Dodger Stadium", 
 	    "div": "NL West", "mgr": "Don Mattingly"}
@@ -48,7 +48,7 @@ dodgers_years = [{ "year": "2013", "wins": 92, "losses": 70, "standing": 1, "pla
 		 { "year": "2005", "wins": 71, "losses": 91, "standing": 4, "playoffs": "", "attend": 3603646, "payroll": 83039000 },
 		 { "year": "2004", "wins": 93, "losses": 69, "standing": 1, "playoffs": "Lost LDS (3-1)", "attend": 3488283, "payroll": 92902001}]
 
-nationals_team = { "id": "4", "name": "Washington Nationals", "social": "@Nationals", "city": "Washington", 
+nationals_team = { "abbr": "WAS", "name": "Washington Nationals", "social": "@Nationals", "city": "Washington", 
 		   "state": "DC", "park": "Nationals Park", "div": "NL East", "mgr": "Matt Williams"} 
 nationals_image = "http://content.sportslogos.net/logos/54/578/thumbs/rcehah9k0kekjkgzm077fflws.gif"     
 nationals_years = [{ "year": "2013", "wins": 86, "losses": 76, "standing": 2, "playoffs": "", "attend": 2652422, "payroll": 112493250 },
@@ -62,7 +62,7 @@ nationals_years = [{ "year": "2013", "wins": 86, "losses": 76, "standing": 2, "p
 	 { "year": "2005", "wins": 81, "losses": 81, "standing": 5, "playoffs": "", "attend": 2731993, "payroll": 48581500 },
 	 { "year": "2004", "wins": 67, "losses": 95, "standing": 5, "playoffs": "", "attend": 749550, "payroll": 41197500}]
 	 
-marlins_team = { "id": "5", "name": "Miami Marlins", "social": "@Marlins", "city": "Miami", 
+marlins_team = { "abbr": "MIA", "name": "Miami Marlins", "social": "@Marlins", "city": "Miami", 
 	    "state": "FL", "park": "Marlins Park", "div": "NL East", "mgr": "Mike Redmond"}
 marlins_image = "http://content.sportslogos.net/logos/54/3637/thumbs/y6oklqzigo1ver57oxlt60ee0.gif"
 marlins_years = [{ "year": "2013", "wins": 62, "losses": 100, "standing": 5, "playoffs": "", "attend": 1586322, "payroll": 24761900 },
@@ -76,7 +76,7 @@ marlins_years = [{ "year": "2013", "wins": 62, "losses": 100, "standing": 5, "pl
 		 { "year": "2005", "wins": 83, "losses": 79, "standing": 3, "playoffs": "", "attend": 1852608, "payroll": 60408834 },
 		 { "year": "2004", "wins": 83, "losses": 79, "standing": 3, "playoffs": "", "attend": 1723105, "payroll": 42143042 }]
 
-twins_team = { "id": "6", "name": "Minnesota Twins", "social": "@Twins", "city": "Minneapolis", 
+twins_team = { "abbr": "MIN", "name": "Minnesota Twins", "social": "@Twins", "city": "Minneapolis", 
 	  "state": "MN", "park": "Target Field", "div": "AL Central", "mgr": "Ron Gardenhire"} 
 twins_image = "http://content.sportslogos.net/logos/53/65/thumbs/peii986yf4l42v3aa3hy0ovlf.gif"
 twins_years = [{ "year": "2013", "wins": 66, "losses": 96, "standing": 4, "playoffs": "", "attend": 2477644, "payroll": 63042500 },
@@ -87,10 +87,10 @@ twins_years = [{ "year": "2013", "wins": 66, "losses": 96, "standing": 4, "playo
 		{ "year": "2008", "wins": 88, "losses": 75, "standing": 2, "playoffs": "", "attend": 2302431, "payroll": 56932766 },
 		{ "year": "2007", "wins": 79, "losses": 83, "standing": 3, "playoffs": "", "attend": 2296383, "payroll": 71439500 },
 		{ "year": "2006", "wins": 96, "losses": 66, "standing": 1, "playoffs": "Lost LDS (3-0)", "attend": 2285018, "payroll": 63396006 },
-		{ "year": "2005", "wins": 83, "losses": 79, "standing": 3, "playoffs": "", "attend": 2034243, "payroll": "$56186000" },
+		{ "year": "2005", "wins": 83, "losses": 79, "standing": 3, "playoffs": "", "attend": 2034243, "payroll": 56186000 },
 		{ "year": "2004", "wins": 92, "losses": 70, "standing": 1, "playoffs": "Lost LDS (3-1)", "attend": 1911490, "payroll": 53890000}]
 
-redsox_team = { "id": "7", "name": "Boston Red Sox", "social": "@RedSox", "city": "Boston", "state": "MA", 
+redsox_team = { "abbr": "BOS", "name": "Boston Red Sox", "social": "@RedSox", "city": "Boston", "state": "MA", 
 	   "park": "Fenway Park", "div": "AL East", "mgr": "John Farrell"}
 redsox_image = "http://content.sportslogos.net/logos/53/53/thumbs/c0whfsa9j0vbs079opk2s05lx.gif"
 redsox_years = [{ "year": "2013", "wins": 97, "losses": 65, "standing": 1, "playoffs": "Won WS (4-2)", "attend": 2833333, "payroll": 177930500 },
@@ -104,7 +104,7 @@ redsox_years = [{ "year": "2013", "wins": 97, "losses": 65, "standing": 1, "play
 		{ "year": "2005", "wins": 95, "losses": 67, "standing": 2, "playoffs": "Lost LDS (3-0)", "attend": 2847888, "payroll": 123505125 },
 		{ "year": "2004", "wins": 98, "losses": 64, "standing": 2, "playoffs": "Won WS (4-0)", "attend": 2837294, "payroll": 127298500}]
 
-yankees_team = { "id": "8", "name": "New York Yankees","city": "New York", "state": "NY", "social": "@Yankees",
+yankees_team = { "abbr": "NYY", "name": "New York Yankees","city": "New York", "state": "NY", "social": "@Yankees",
 	    "park": "Yankee Stadium III", "div": "AL East", "mgr": "Joe Girardi"}
 yankees_image = "http://content.sportslogos.net/logos/53/68/thumbs/1256.gif", 
 yankees_years = [{ "year": "2013", "wins": 85, "losses": 77, "standing": 3, "playoffs": "", "attend": 3279589, "payroll": 246044750 },
@@ -118,7 +118,7 @@ yankees_years = [{ "year": "2013", "wins": 85, "losses": 77, "standing": 3, "pla
 	 { "year": "2005", "wins": 95, "losses": 67, "standing": 1, "playoffs": "Lost LDS (3-2)", "attend": 4090696, "payroll": 208306817 },
 	 { "year": "2004", "wins": 101, "losses": 61, "standing": 1, "playoffs": "Lost ALCS (4-3)", "attend": 3775292, "payroll": 184193950}]
 
-pirates_team = { "id": "9", "name": "Pittsburgh Pirates", "social": "@Pirates", "city": "Pittsburgh", "state": "PA", 
+pirates_team = { "abbr": "PIT", "name": "Pittsburgh Pirates", "social": "@Pirates", "city": "Pittsburgh", "state": "PA", 
 	    "park": "PNC Park", "div": "NL Central", "mgr": "Clint Hurdle"}
 pirates_image = "http://content.sportslogos.net/logos/54/71/thumbs/7112502014.gif"
 pirates_years = [{ "year": "2013", "wins": 94, "losses": 68, "standing": 2, "playoffs": "Lost LDS (3-2)", "attend": 2256862, "payroll": 96205000 },
@@ -132,7 +132,7 @@ pirates_years = [{ "year": "2013", "wins": 94, "losses": 68, "standing": 2, "pla
 		 { "year": "2005", "wins": 67, "losses": 95, "standing": 6, "playoffs": "", "attend": 1817245, "payroll": 38133000 },
 		 { "year": "2004", "wins": 72, "losses": 89, "standing": 5, "playoffs": "", "attend": 1580031, "payroll": 32227929}]
 		 
-tigers_team = { "id": "10", "name": "Detroit Tigers", "social": "@Tigers", "city": "Detroit", "state": "MI", 
+tigers_team = { "abbr": "DET", "name": "Detroit Tigers", "social": "@Tigers", "city": "Detroit", "state": "MI", 
 	   "park": "Comerica Park", "div": "AL Central", "mgr": "Brad Ausmus"} 
 tigers_image = "http://content.sportslogos.net/logos/53/59/thumbs/txtu234jlffk0q5l62uhnwm3q.gif"
 tigers_years = [{ "year": "2013", "wins": 93, "losses": 69, "standing": 1, "playoffs": "Lost ALCS (4-2)", "attend": 3083397, "payroll": 154407000 },
@@ -146,6 +146,21 @@ tigers_years = [{ "year": "2013", "wins": 93, "losses": 69, "standing": 1, "play
 	 { "year": "2005", "wins": 71, "losses": 91, "standing": 4, "playoffs": "", "attend": 2024431, "payroll": 69092000 },
 	 { "year": "2004", "wins": 72, "losses": 90, "standing": 4, "playoffs": "", "attend": 1917004, "payroll": 46832000}]
 
+rangers_team = { "abbr": "TEX", "name": "Texas Rangers", "social": "@Rangers", "city": "Dallas", "state":"TX",
+	 "park":"Rangers Ballpark", "div": "AL West", "mgr": "Ron Washington"}
+rangers_image = "http://content.sportslogos.net/logos/53/77/thumbs/ajfeh4oqeealq37er15r3673h.gif"
+rangers_years = [{ "year": "2013", "wins": 91, "losses": 72, "standing": 2, "playoffs": "", "attend": 3178273, "payroll": 138281200 },
+	{ "year": "2012", "wins": 93, "losses": 69, "standing": 2, "playoffs": "Lost ALWC (1-0)", "attend": 3460280, "payroll": 124119900 },
+	{ "year": "2011", "wins": 96, "losses": 66, "standing": 1, "playoffs": "Lost WS (4-3)", "attend": 2946949, "payroll": 93799264 },
+	{ "year": "2010", "wins": 90, "losses": 72, "standing": 1, "playoffs": "Lost WS (4-1)", "attend": 2505171, "payroll": 56474374 },
+	{ "year": "2009", "wins": 87, "losses": 75, "standing": 2, "playoffs": "", "attend": 2156016, "payroll": 79723548 },
+	{ "year": "2008", "wins": 79, "losses": 83, "standing": 2, "playoffs": "", "attend": 1945677, "payroll": 68037326 },
+	{ "year": "2007", "wins": 75, "losses": 87, "standing": 4, "playoffs": "", "attend": 2353862, "payroll": 68643675 },
+	{ "year": "2006", "wins": 80, "losses": 82, "standing": 3, "playoffs": "", "attend": 2388757, "payroll": 68228662 },
+	{ "year": "2005", "wins": 79, "losses": 83, "standing": 3, "playoffs": "", "attend": 2525221, "payroll": 55849000 },
+	{ "year": "2004", "wins": 89, "losses": 73, "standing": 3, "playoffs": "", "attend": 2513685, "payroll": 55050417 }]
+
+
 angels = dict(team=angels_team,image=angels_image,years=angels_years)
 cardinals = dict(team=cardinals_team, image=cardinals_image, years=cardinals_years)
 dodgers = dict(team=dodgers_team, image=dodgers_image, years=dodgers_years)
@@ -156,6 +171,7 @@ redsox = dict(team=redsox_team, image=redsox_image, years=redsox_years)
 yankees = dict(team=yankees_team, image=yankees_image, years=yankees_years)
 pirates = dict(team=pirates_team, image=pirates_image, years=pirates_years)
 tigers = dict(team=tigers_team, image=tigers_image, years=tigers_years)
+rangers = dict(team=rangers_team, image=rangers_image, years=rangers_years)
 
-teams = [angels, cardinals, dodgers, nationals, marlins, twins, redsox, yankees, pirates, tigers]
+teams = [angels, cardinals, dodgers, nationals, marlins, twins, redsox, yankees, pirates, tigers, rangers]
 	 
