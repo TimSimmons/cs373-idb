91756cbe612c559a30201b7b608ac6fadc363770
Jordan Graves <jgraves8205@aol.com>
2014-04-02 19:38:14 -0500

    add post for players, need to test

--- idb/api.py
+++ idb/api.py
@@ -6,23 +6,34 @@ from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
 def player(request, player_id):
-  player = Player.objects.get(id=player_id)
+  # GET
   if request.method == 'GET':
+    player = Player.objects.get(id=player_id)
     response = HttpResponse(serializers.serialize('json', [ player, ]), content_type="application/json")
+  # PUT
   if request.method == 'PUT':
+    player = Player.objects.get(id=player_id)
     body = json.loads(request.body)
     for k,v in body.items():
       setattr(player, k, v)
       player.save()  
     response = HttpResponse(serializers.serialize('json', [ player, ]), content_type="application/json")
+  #POST
+  if request.method == 'POST':
+    body = json.loads(request.body)
+    player = Player(**body)
+    response = HttpResponse(serializers.serialize('json', [ player, ]), content_type="application/json")
   return response
   
+
 @csrf_exempt
 def team(request, team_id):
   team = Team.objects.get(id=team_id)
   if request.method == 'GET':
     response = HttpResponse(serializers.serialize('json', [ team, ]), content_type="application/json")
   return response
+ 
+
  
 @csrf_exempt
 def year(request, year_id):
