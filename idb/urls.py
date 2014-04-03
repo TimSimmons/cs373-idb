426dd68e65c0a0dbe8c096a3405aff6d5730ce49
Tim Simmons <tim.r.simmons@gmail.com>
2014-04-02 21:36:34 -0500

    Update views/urls to only use the DB

--- idb/urls.py
+++ idb/urls.py
@@ -1,20 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
-from idb.views import home, player, team, year, player_model, team_model, year_model
+from idb.views import home, player, team, year, team_abbr
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'idb.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
     url(r'^$', home),
-    url(r'^players/(\d{2})/$', player),
-    url(r'^players_model/(\d{1})/$', player_model),
-    url(r'^teams_model/(\d{1})/$', team_model),
-    url(r'^teams/([A-Za-z]{3})/$', team),
+    url(r'^players/(\d*)/$', player),
+    url(r'^teams/(\d*)/$', team),
+    url(r'^teams/([A-Za-z]{3})/$', team_abbr),
     url(r'^years/(\d{4})/$', year),
-    url(r'^years_model/(\d{4})/$', year_model),
     url(r'^admin/', include(admin.site.urls)),
 )
 
