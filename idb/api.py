64af55326bfc40a784a284872d5f033046ce148d
Jordan Graves <jgraves8205@aol.com>
2014-04-02 19:34:18 -0500

    added get for teams, years, players / added put for players

--- idb/api.py
+++ idb/api.py
@@ -2,9 +2,31 @@ from django.http import HttpResponse, Http404
 from django.core import serializers
 from idb.models import *
 import json
+from django.views.decorators.csrf import csrf_exempt
 
-def api_player(request, player_id):
+@csrf_exempt
+def player(request, player_id):
   player = Player.objects.get(id=player_id)
   if request.method == 'GET':
     response = HttpResponse(serializers.serialize('json', [ player, ]), content_type="application/json")
+  if request.method == 'PUT':
+    body = json.loads(request.body)
+    for k,v in body.items():
+      setattr(player, k, v)
+      player.save()  
+    response = HttpResponse(serializers.serialize('json', [ player, ]), content_type="application/json")
+  return response
+  
+@csrf_exempt
+def team(request, team_id):
+  team = Team.objects.get(id=team_id)
+  if request.method == 'GET':
+    response = HttpResponse(serializers.serialize('json', [ team, ]), content_type="application/json")
+  return response
+ 
+@csrf_exempt
+def year(request, year_id):
+  year = Year.objects.get(year=year_id)
+  if request.method == 'GET':
+    response = HttpResponse(serializers.serialize('json', [ year, ]), content_type="application/json")
   return response
